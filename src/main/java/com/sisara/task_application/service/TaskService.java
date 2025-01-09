f7833caa681b6306db45b8a51a6c1265375bf7f9
N.S.D Mahaliyana <141614170+nipunasahanmahaliyana@users.noreply.github.com>
2025-01-09 22:00:02 +0530

    crud updated

--- src/main/java/com/sisara/task_application/service/TaskService.java
+++ src/main/java/com/sisara/task_application/service/TaskService.java
@@ -1,7 +1,7 @@
 package com.sisara.task_application.service;
 
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.sisara.task_application.dto.TaskDto;
+import com.sisara.task_application.exception.TaskNotExistException;
 import com.sisara.task_application.model.Task;
 import com.sisara.task_application.repository.TaskRepository;
 
@@ -34,5 +35,32 @@ public class TaskService {
         Task savedTask = taskRepository.save(task);
 
         return modelMapper.map(savedTask, TaskDto.class);
+    }
+
+    public TaskDto getTaskbyId(Long id){
+        if (!taskRepository.existsById(id))
+            throw new TaskNotExistException("Task id is invalid " + id);
+
+        Optional<Task> optionalTask = taskRepository.findById(id);
+        return (modelMapper.map(optionalTask.get() ,TaskDto.class));
+    }
+
+    public  TaskDto updateTask(Long id,TaskDto taskDto){
+
+        Task existingTask = taskRepository.findById(id)
+            .orElseThrow(() -> new TaskNotExistException("Task not found with id: " + id));
+
+        modelMapper.map(taskDto, existingTask);
+        Task updatedTask = taskRepository.save(existingTask);
+        return modelMapper.map(updatedTask, TaskDto.class);
+    }
+
+    public  TaskDto deleteTask(Long id){
+        if(!taskRepository.existsById(id))
+            throw new TaskNotExistException("Task is not found"+id);
+        
+        Optional<Task> optionalTask = taskRepository.findById(id);
+        taskRepository.deleteById(id);
+        return (modelMapper.map(optionalTask.get() ,TaskDto.class));
     }
 }
