71f458eaee9e5b4d0e3ba375a468491b49a07130
N.S.D Mahaliyana <141614170+nipunasahanmahaliyana@users.noreply.github.com>
2025-01-09 15:24:09 +0530

    H2 db,task model

---
+++ src/main/java/com/sisara/task_application/model/Task.java
@@ -0,0 +1,39 @@
+package com.sisara.task_application.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String tittle;
+    private String description;
+    private Status status;
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void createdAt(){
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public enum Status {
+        PENDING,IN_PROGRESS,COMPLETED
+    }
+
+}
